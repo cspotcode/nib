342d7d960967de7feae226363b6eaa9f77393a7f
Andrew Bradley <abradley@brightcove.com>
2015-06-20 22:51:12 -0400

    Testing travis stuff

--- travis-after-success.sh
+++ travis-after-success.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 set -e ; set -u
 
-echo "after-success script for branch: $TRAVIS_BRANCH"
+echo "after-success script for branch: $TRAVIS_BRANCH, job #: $TRAVIS_JOB_NUMBER"
 # Build and deploy documentation, but only for the master branch and for the first job
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_JOB_NUMBER == '1' ]]
+IS_FIRST_RE="\.1$"
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_JOB_NUMBER =~ $IS_FIRST_RE ]]
 then
   echo "Building and deploying website"
   SOURCE_DIR="$PWD"
   # Compile the website with roots
   pushd site
-  npm install -g roots
-  roots compile -e production
+  npm install
+  npm run-script build
   # Commit the updated site to gh-pages
   pushd public
   git init
   git config user.name "Travis CI"
   git config user.email "cspotcode.travis@gmail.com"
-  git add -u .
+  git add .
   git commit -m "Deploy to GitHub Pages"
 
   # Set up ssh access to github
@@ -27,9 +28,11 @@ Host github
   IdentityFile "$SOURCE_DIR/id_rsa"
   IdentitiesOnly yes
 EOF
+  chmod 600 ~/.ssh/config
 
   # Push to gh-pages.  This destroys all history for the gh-pages branch.
-  git push --force --quiet "ssh://github:${GH_REF}" master:gh-pages
+  echo "pushing to github:${GH_REF}"
+  git push --force --quiet "github:${GH_REF}" master:gh-pages
 else
   echo "Not building or deploying website"
 fi
